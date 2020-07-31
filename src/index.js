0411e621dbc79d404d54352e68dc86d9a7f1978d
Mohammad Mohajer <55400075+mmmohajer@users.noreply.github.com>
2020-07-31 11:45:24 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./style.scss";
+
+import DraggableUploader from "./draggableUploader";
+
+ReactDOM.render(
+	<DraggableUploader/>, 
+	document.getElementById("root")
+	);
